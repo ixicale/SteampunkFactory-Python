dc8739c708145343c93ed2f291bff0683962f6e7
ixicale <isaiasxicaleescalante@gmail.com>
2019-02-26 16:09:34 -0500

    scaffolding data

--- Pandas/Commands/ShowVersions.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-
-#print("pd.__version__")
-#print(pd.__version__)
-print("pd.show_version")
-print(pd.show_versions(as_json=False))
--- flask_test_multinode/run.py
+++
@@ -1,5 +0,0 @@
-# importa la variable que tenga todas las rutas asignadas
-from lib.dispatcher import app
-
-# ejecuta el programa con flask del objeto dispatcher.py
-app.run(port=2323, debug=True)
