2b2b394b4ae35dfb30967e3739071791e3e3dee9
ixicale <isaiasxicaleescalante@gmail.com>
2019-04-17 09:39:20 -0500

    requests

--- Basic/while_request.py
+++ Basic/while_request.py
@@ -4,14 +4,15 @@ import json
 import threading
 import time
 
-url = "http://dev.clever-api-fin.local/proxycharge/pendingcharges"
+url = "http://10.8.17.26:23"
 def link ():
 
     response = requests.get(url)
-    print(response.content)
-    
+    print(response)
+
 while 1:
     for i in range(1001):
         t = threading.Thread(target=link)
         t.start()
-    time.sleep(1)
+        print(url)
+        time.sleep(1)
