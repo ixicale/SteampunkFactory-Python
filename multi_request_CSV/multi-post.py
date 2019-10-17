e880b4a6b062805956de30b1b5545c17b2215bd1
ixicale <ixicaleescalante@gmail.com>
2019-10-17 09:35:04 -0500

    multi-post

--- multi_request_CSV/multi-post.py
+++ multi_request_CSV/multi-post.py
@@ -18,11 +18,12 @@ File Not Found ({FNAME}).
 Check the file and restart again
 """
 csv.register_dialect(
-        'myDialect',
-        delimiter='\t',
-        quotechar='"',
-        quoting=csv.QUOTE_ALL,
-        skipinitialspace=True)
+    "myDialect",
+    delimiter="\t",
+    quotechar='"',
+    quoting=csv.QUOTE_ALL,
+    skipinitialspace=True,
+)
 
 
 def get_file_content(fname):
@@ -38,8 +39,11 @@ def get_file_content(fname):
 REQUESTS = get_file_content("requests.txt").splitlines()
 PROPE = get_file_content("properties.txt").splitlines()
 NOW = datetime.now()
-FNAME = os.path.abspath(os.path.dirname(__file__)) \
-    + "/resp/" + NOW.strftime("%Y%m%d_%H%M%S_rates.csv")
+FNAME = (
+    os.path.abspath(os.path.dirname(__file__))
+    + "/resp/"
+    + NOW.strftime("%Y%m%d_%H%M%S_rates.csv")
+)
 QA = "http://rates-api-qa.clever.palace-resorts.local"
 API_ENDPOINT = QA + "/rateview/levels"
 TITLES = [
@@ -61,23 +65,65 @@ TITLES = [
     "MIN_POR_NVL",
     "Error",
     "response error",
-
 ]
 
 
 def writeCSV():
     print("creating: ", FNAME)
-    with open(FNAME, 'w') as csvFile:
-        writer = csv.writer(csvFile, dialect='myDialect')
+    with open(FNAME, "w") as csvFile:
+        writer = csv.writer(csvFile, dialect="myDialect")
         writer.writerow(TITLES)
     csvFile.close()
+
+
+def build_row(
+    NO_PAYLOAD="",
+    PAYLOAD="",
+    FB_REQUEST="",
+    FE_REQUEST="",
+    F_INICIO_RESPONSE="",
+    F_FIN_RESPONSE="",
+    PROP="",
+    TARIFA=False,
+    NIVEL=False,
+    JSON_NIVELES="",
+    NUM_ITEM="",
+    NUM_tARIFA="",
+    NUM_NIVEL="",
+    AMOUNT="",
+    MAX_POR_NVL="",
+    MIN_POR_NVL="",
+    Error="",
+    response_error="",
+):
+
+    return [
+        NO_PAYLOAD,
+        PAYLOAD,
+        FB_REQUEST,
+        FE_REQUEST,
+        F_INICIO_RESPONSE,
+        F_FIN_RESPONSE,
+        PROP,
+        TARIFA,
+        NIVEL,
+        JSON_NIVELES,
+        NUM_ITEM,
+        NUM_tARIFA,
+        NUM_NIVEL,
+        AMOUNT,
+        MAX_POR_NVL,
+        MIN_POR_NVL,
+        Error,
+        response_error,
+    ]
 
 
 def appendCSV(row):
     # print(row)
 
-    with open(FNAME, 'a') as csvFile:
-        writer = csv.writer(csvFile, dialect='myDialect')
+    with open(FNAME, "a") as csvFile:
+        writer = csv.writer(csvFile, dialect="myDialect")
         writer.writerow(row)
     csvFile.close()
 
@@ -119,8 +165,7 @@ def subProcess(propiedad, index, data):
         if not resp["error"] & (type(resp["data"]) is not None):
 
             # print(resp, pastebin_url, end="\n__________________\n", sep="\n")
-            
-                
+
             dataList = resp["data"]
             data_p = 0
             for data in dataList:
@@ -143,102 +188,82 @@ def subProcess(propiedad, index, data):
                             lvl_p = 0
                             for lvl in JSON_NIVELES:
                                 lvl_p = lvl_p + 1
-                                appendCSV([
-                                    NO_PAYLOAD,
-                                    PAYLOAD,
-                                    val["travelBegin"],
-                                    val["travelEnd"],
-                                    F_INICIO,
-                                    F_FIN,
-                                    PROP,
-                                    TARIFA,
-                                    NIVEL,
-                                    JSON_NIVELES,
-                                    data_p,
-                                    tarifa_p,
-                                    lvl_p,
-                                    Amount,
-                                    lvl["max"],
-                                    lvl["min"]
-                                ])
+                                appendCSV(
+                                    build_row(
+                                        NO_PAYLOAD=NO_PAYLOAD,
+                                        PAYLOAD=PAYLOAD,
+                                        FB_REQUEST=val["travelBegin"],
+                                        FE_REQUEST=val["travelEnd"],
+                                        F_INICIO_RESPONSE=F_INICIO,
+                                        F_FIN_RESPONSE=F_FIN,
+                                        TARIFA=TARIFA,
+                                        NIVEL=NIVEL,
+                                        JSON_NIVELES=JSON_NIVELES,
+                                        NUM_ITEM=data_p,
+                                        NUM_tARIFA=tarifa_p,
+                                        NUM_NIVEL=lvl_p,
+                                        AMOUNT=Amount,
+                                        MAX_POR_NVL=lvl["max"],
+                                        MIN_POR_NVL=lvl["min"],
+                                        PROP=PROP,
+                                    )
+                                )
                         else:
-                            appendCSV([
-                                NO_PAYLOAD,
-                                PAYLOAD,
-                                val["travelBegin"],
-                                val["travelEnd"],
-                                F_INICIO,
-                                F_FIN,
-                                PROP,
-                                TARIFA,
-                                NIVEL,
-                                JSON_NIVELES,
-                                data_p,
-                                tarifa_p,
-                                "",
-                                Amount,
-                                "",
-                                ""
-                            ])
+                            appendCSV(
+                                build_row(
+                                    NO_PAYLOAD=NO_PAYLOAD,
+                                    PAYLOAD=PAYLOAD,
+                                    FB_REQUEST=val["travelBegin"],
+                                    FE_REQUEST=val["travelEnd"],
+                                    F_INICIO_RESPONSE=F_INICIO,
+                                    F_FIN_RESPONSE=F_FIN,
+                                    TARIFA=TARIFA,
+                                    NIVEL=NIVEL,
+                                    JSON_NIVELES=JSON_NIVELES,
+                                    NUM_ITEM=data_p,
+                                    NUM_tARIFA=tarifa_p,
+                                    AMOUNT=Amount,
+                                    PROP=PROP,
+                                )
+                            )
                 else:
-                    appendCSV([
-                        NO_PAYLOAD,
-                        PAYLOAD,
-                        val["travelBegin"],
-                        val["travelEnd"],
-                        F_INICIO,
-                        F_FIN,
-                        PROP,
-                        TARIFA,
-                        False,
-                        "",
-                        data_p,
-                        "",
-                        "",
-                        "",
-                        "",
-                        ""
-                    ])
+                    appendCSV(
+                        build_row(
+                            NO_PAYLOAD=NO_PAYLOAD,
+                            PAYLOAD=PAYLOAD,
+                            FB_REQUEST=val["travelBegin"],
+                            FE_REQUEST=val["travelEnd"],
+                            F_INICIO_RESPONSE=F_INICIO,
+                            F_FIN_RESPONSE=F_FIN,
+                            TARIFA=TARIFA,
+                            NUM_ITEM=data_p,
+                            PROP=PROP,
+                        )
+                    )
         else:
-            appendCSV([
-                NO_PAYLOAD,
-                PAYLOAD,
-                val["travelBegin"],
-                val["travelEnd"],
-                "",
-                "",
-                propiedad,
-                False,
-                False,
-                "",
-                "",
-                "",
-                "",
-                "",
-                lvl["max"],
-                lvl["min"]
-            ])
+            appendCSV(
+                build_row(
+                    NO_PAYLOAD=NO_PAYLOAD,
+                    PAYLOAD=PAYLOAD,
+                    FB_REQUEST=val["travelBegin"],
+                    FE_REQUEST=val["travelEnd"],
+                    MAX_POR_NVL=lvl["max"],
+                    MIN_POR_NVL=lvl["min"],
+                    PROP=PROP,
+                )
+            )
     except Exception as e:
-        appendCSV([
-            NO_PAYLOAD,
-            PAYLOAD,
-            val["travelBegin"],
-            val["travelEnd"],
-            "",
-            "",
-            "",
-            False,
-            False,
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            str(e),
-            r.text
-        ])
+
+        appendCSV(
+            build_row(
+                NO_PAYLOAD=NO_PAYLOAD,
+                PAYLOAD=PAYLOAD,
+                FB_REQUEST=val["travelBegin"],
+                FE_REQUEST=val["travelEnd"],
+                Error=str(e),
+                response_error=r.text,
+            )
+        )
 
 
 def requester():
@@ -249,10 +274,20 @@ def requester():
             k = k + 1
             for index in range(0, 53):
                 print(k, propiedad, index)
-                t = threading.Thread(target=subProcess, args=(propiedad, index, data,))
-                t.start()
-                if index%10 == 0:
-                    time.sleep(0.75)
+                try:
+                    t = threading.Thread(
+                        target=subProcess, args=(propiedad, index, data)
+                    )
+                    t.start()
+                    if index % 10 == 0:
+                        time.sleep(1.45)
+                except Exception:
+                    time.sleep(0.5)
+
+                    t = threading.Thread(
+                        target=subProcess, args=(propiedad, index, data)
+                    )
+                    t.start()
 
 
 requester()
